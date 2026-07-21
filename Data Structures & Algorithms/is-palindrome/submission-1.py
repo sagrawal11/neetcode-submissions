1f508beb34f0f65fb24032fcf4888b6886c05dfe
Sarthak Agrawal <129307798+sagrawal11@users.noreply.github.com>
2026-07-21 17:27:44 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # we could be given anything, and we only want alphanumerics
+        # also it's easier to compare consistently when we lowercase and squish everything together
+        cleaned_s = [char.lower() for char in s if char.isalnum()]
+        
+        # two pointers to go through the phrase
+        #left = 0
+        #right = len(cleaned_s) - 1
+
+        #while left < right:
+            #if cleaned_s[left] != cleaned_s[right]:
+                #return False
+            #left += 1
+            #right -= 1
+    
+        #return True
+
+        # we don't need to use pointers we can just reverse the string and check
+        if cleaned_s == cleaned_s[::-1]:
+            return True
+        return False
